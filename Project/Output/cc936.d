bd036a582773af4ccae0ddab429baa66eaae6358
seekskywang <seekskywang@gmail.com>
2023-03-24 08:32:44 +0800

    1

---
+++ Project/Output/cc936.d
@@ -0,0 +1,17 @@
+.\output\cc936.o: ..\User\FATFS_V0.08A\src\cc936.c
+.\output\cc936.o: ..\User\FATFS_V0.08A\src\ff.h
+.\output\cc936.o: ..\User\FATFS_V0.08A\src\integer.h
+.\output\cc936.o: ..\User\FATFS_V0.08A\src\ffconf.h
+.\output\cc936.o: ..\User\Host\usbhost_lpc17xx.h
+.\output\cc936.o: ..\User\Include\usbhost_inc.h
+.\output\cc936.o: ..\Libraries\Core\CM3\DeviceSupport\NXP\LPC177x_8x\LPC177x_8x.h
+.\output\cc936.o: ..\Libraries\Core\CM3\CoreSupport\core_cm3.h
+.\output\cc936.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\output\cc936.o: ..\Libraries\Core\CM3\CoreSupport\core_cmInstr.h
+.\output\cc936.o: ..\Libraries\Core\CM3\CoreSupport\core_cmFunc.h
+.\output\cc936.o: ..\User\Include\usbhost_err.h
+.\output\cc936.o: ..\User\Include\../Host/usbhost_lpc17xx.h
+.\output\cc936.o: ..\User\Include\../MassStorage/usbhost_ms.h
+.\output\cc936.o: ..\User\Include\usbhost_inc.h
+.\output\cc936.o: ..\User\Include\../Fat/usbhost_fat.h
+.\output\cc936.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
